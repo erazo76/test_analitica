cf995bfe9edcda2a2d75d95dcd47218300687f86
Gustavo erazo <erazo.gustavo@gmail.com>
2021-02-24 00:43:27 -0500

    consultando servicio

--- index.php
+++ index.php
@@ -1,12 +1,29 @@
-<!DOCTYPE html>
-<html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Test Analitica</title>
-</head>
-<body>
-    
-</body>
-</html>
+<?php
+
+$options = array();
+$options['location'] = 'http://35.211.83.38/AZDigitalV6/WebServices/ServiciosAZDigital.wsdl';
+$options['uri'] = 'http://35.211.83.38/AZDigitalV6/WebServices/SOAP/';
+$options['cache_wsdl'] = WSDL_CACHE_NONE;
+$options['soap_version'] = SOAP_1_2;
+
+$cliente = new SoapClient('http://35.211.83.38/AZDigitalV6/WebServices/ServiciosAZDigital.wsdl?WSDL',$options);
+						  
+					
+$parametros = array('Tipo' => 'FechaInicio', 'Expersion' => '2019-07-01 00:00:00');
+
+$resultado = $cliente->BuscarArchivo(array('Condiciones' =>array('Condicion' => $parametros)));  /*$cliente ->__soapCall("BuscarArchivo", $parametros);*/
+
+if ($cliente->fault) {
+	echo 'Fallo';
+	print_r($resultado);
+} else {	// Chequea errores
+	$err = $cliente->getError();
+	if ($err) {		// Muestra el error
+		echo 'Error' . $err ;
+	} else {		// Muestra el resultado
+		echo 'Resultado';
+		print_r ($resultado);
+	}
+}
+
+?>
